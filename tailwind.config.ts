f39dca441dd740cf2ce8a22805c02a26e56d40fc
Tae-sung-Kim <qwasszx82@gmail.com>
2025-06-18 11:57:14 +0900

    second commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}", // Next.js 13 이상일 때
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // Next.js 13 이상일 때
   ],
   theme: {
     extend: {},
