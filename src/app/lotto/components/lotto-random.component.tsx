fb63a358259f2ce1fe90178323651893748430b3
Tae-sung-Kim <qwasszx82@gmail.com>
2025-06-22 09:49:32 +0900

    개발 진행중

---
+++ src/app/lotto/components/lotto-random.component.tsx
@@ -0,0 +1,7 @@
+export default function LottoRandomComponent() {
+  return (
+    <div>
+      <h1>랜덤로또 번호 생성</h1>
+    </div>
+  );
+}
---
+++ src/app/lotto/page.tsx
@@ -0,0 +1,10 @@
+import LottoRandomComponent from './components/lotto-random.component';
+
+export default function LottoPage() {
+  return (
+    <div>
+      <h1>로또번호</h1>
+      <LottoRandomComponent />
+    </div>
+  );
+}
