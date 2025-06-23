ab58b9fa03131f53682b0838f9619355f5728bc7
Tae-sung-Kim <qwasszx82@gmail.com>
2025-06-23 13:13:24 +0900

    로또 번호랜덤 추출

--- src/app/lotto/components/lotto-random.component.tsx
+++ src/app/lotto/components/lotto-random.component.tsx
@@ -1,14 +1,61 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import { Counter } from '@/components/counter';
+import { useCallback, useState } from 'react';
 
 export default function LottoRandomComponent() {
-  const handleRandomLottoNumber = () => {
-    console.log('1111');
+  const [orderCount, setOrderCount] = useState(1);
+  const [lottoNumberList, setLottoNumberList] = useState<number[][]>([]);
+
+  // 구매 수량 결정
+  const handleOrderCountChange = (newValue: number) => {
+    setOrderCount(newValue);
   };
 
+  // 구매 수량에 맞게 랜덤번호 생성
+  const handleRandomLottoNumber = useCallback(() => {
+    const lottoNumberList: number[][] = [];
+
+    // 중복되지 않는 6개의 숫자 생성
+    const generateLottoNumbers = (): number[] => {
+      const numbers = new Set<number>();
+      while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+      }
+      return Array.from(numbers).sort((a, b) => a - b);
+    };
+
+    Array.from({ length: orderCount }, () => {
+      const unique6 = generateLottoNumbers();
+
+      lottoNumberList.push(unique6);
+    });
+
+    setLottoNumberList(lottoNumberList);
+  }, [orderCount]);
+
   return (
-    <Button variant="outline" onClick={handleRandomLottoNumber}>
-      랜덤번호 생성11
-    </Button>
+    <div className="space-y-4 p-4">
+      <div className="flex items-center gap-4">
+        <Counter
+          value={orderCount}
+          min={1}
+          max={99}
+          onChange={handleOrderCountChange}
+          label="구매 수량"
+        />
+        <Button onClick={handleRandomLottoNumber}>
+          {orderCount}개 번호 생성
+        </Button>
+      </div>
+
+      <ul className="list-disc">
+        {lottoNumberList.map((d) => (
+          <li className="m-10" key={d.toString()}>
+            {d.join(', ')}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
