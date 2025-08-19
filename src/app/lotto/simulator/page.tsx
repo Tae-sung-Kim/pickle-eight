c105fce1634e6e979a2c1ac9721840aa1a176e0e
Tae-sung-Kim <qwasszx82@gmail.com>
2025-08-19 09:55:59 +0900

    빌드 오류 수정

--- src/app/lotto/simulator/page.tsx
+++ src/app/lotto/simulator/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import React, { useEffect, useMemo, useState } from 'react';
-import { LottoGenerator } from '@/utils/lotto-generator.util';
-import { LottoUtils } from '@/utils/lotto.util';
+import { LottoUtils, LottoGenerator } from '@/utils';
 
 function Ball({ n }: { n: number }) {
   return (
